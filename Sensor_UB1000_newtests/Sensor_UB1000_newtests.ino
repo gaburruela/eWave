efcb733d1eefb1b274006c5606b931b6fc95a3ff
AndresJimenezPiedra <andres11880jp@estudiantec.cr>
2024-10-23 16:43:39 -0600

    Added calibration variables for both sensors into the same arduino file

--- Sensor_UB1000_newtests/Sensor_UB1000_newtests.ino
+++ Sensor_UB1000_newtests/Sensor_UB1000_newtests.ino
@@ -1,34 +1,62 @@
 // Se toman n mediciones y se saca el promedio de todas esas, dura como 3 segundos cada promedio
 
-const int sensor = A0;
-const int num_med = 1;
+const int sensor1 = A4;
+const int sensor2 = A0;  (Bond)
+const int num_med = 69; //nice
 
-float voltage;
+float s1_voltage;
+float s2_voltage;
 float distance;
-float distance_avg = 0;
-float distance_calibrated = 0;
+float s1_distance_avg = 0;
+float s2_distance_avg = 0;
+float s1_distance_calibrated = 0;
+float s2_distance_calibrated = 0;
+
+// Manual mapping sensor 1
+float s1_min_Volt = 162;
+float s1_max_Volt = 813;
+float s1_min_Dist = 89;
+float s1_max_Dist = 976;
+
+// Manual mapping sensor 2 (Bond)
+float s2_min_Volt = 158;
+float s2_max_Volt = 794;
+float s2_min_Dist = 85;
+float s2_max_Dist = 967;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(sensor, INPUT);
+  // pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
 }
 
 void loop() {
   // Hace las n mediciones y las suma
-  distance_avg = 0; // Hacer reset por favor!
+  // s1_distance_avg = 0; // Hacer reset por favor!
+  s2_distance_avg = 0; 
   for (int i = 0; i < num_med; i++) {
-    voltage = analogRead(sensor);
-    distance_avg = distance_avg + voltage / 1024 * 930 + 70; // Mapeo manual para tener decimales (regla de 3)
-    // distance_avg = map(voltage,0,1024,30,500); // Mapeo automático de Arduino
+    // s1_voltage = analogRead(sensor1);
+    s2_voltage = analogRead(sensor2);
+    // Serial.print("voltaje sensor 1: ");
+    // Serial.print(s1_voltage);
+    Serial.print(" voltaje sensor 2: ");
+    Serial.println(s2_voltage);
+    // s1_distance_avg = s1_distance_avg + (s1_voltage - s1_min_Volt) / (s1_max_Volt - s1_min_Volt) * (s1_max_Dist - s1_min_Dist) + s1_min_Dist; // Mapeo manual sensor 1 para tener decimales (regla de 3)
+    s2_distance_avg = s2_distance_avg + (s2_voltage - s2_min_Volt) / (s2_max_Volt - s2_min_Volt) * (s2_max_Dist - s2_min_Dist) + s2_min_Dist; // Mapeo manual sensor 2 para tener decimales (regla de 3)
+    // s1_distance_avg = map(s1_voltage,0,1024,30,500); // Mapeo automático de Arduino
     delay(50);
   }
+  
+  // s1_distance_avg = s1_distance_avg / num_med; // Saca el promedio
+  s2_distance_avg = s2_distance_avg / num_med; // Saca el promedio
 
-  distance_avg = distance_avg / num_med; // Saca el promedio
+  // Calibración hecha en el excel: NotBond
+  //s1_distance_calibrated = s1_distance_avg*1.016270686 - 19.43567117;
+  //Serial.println(s1_distance_avg);
+  //Serial.println(s1_distance_calibrated);
 
-  // Calibración hecha en el excel
-  //distance_calibrated = distance_avg/1.0629 + 21.48;
-  Serial.println(distance_avg);
-  //Serial.println(distance_calibrated);
+    // Calibración hecha en el excel: Bond
+  //s2_distance_calibrated = s2_distance_avg*1.016270686 - 19.43567117;
+  Serial.println(s2_distance_avg);
+  //Serial.println(s2_distance_calibrated);
 }
-
-// Desfase de ~20 mm experimentales (para obtener medidas reales)
