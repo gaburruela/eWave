19e33200dec36114d1f02dfbf8f5bdac6449d44b
AndresJimenezPiedra <andres11880jp@estudiantec.cr>
2024-11-26 12:02:05 -0600

    Code final version v1

---
+++ General Code/All_the_code/All_the_code.ino
@@ -0,0 +1,420 @@
+// Código unificado general :)
+
+// BUTTON - Change from zero leveling (0) to inductive (1) to everything else (2)
+int button_counter = 3;
+int button_pin = 10;
+int button_rst = 11;
+
+
+// ACCELEROMETER MPU6050 - I2C
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+// Get new sensor events with the readings
+sensors_event_t a, g, temp;
+
+
+// INDUCTIVE
+const int ind_pin = 8; // Digital pin connected to the infrared/inductive sensor
+const int ind_measurements = 3; // Number of measurements to average
+
+bool ind_firstPulseDetected = false; // Flag to check if the first pulse has been detected (only used once)
+unsigned long ind_lastPulseTime = 0; // Time of the last detected signal
+unsigned long ind_currentPulseTime = 0; // Time of the current detected signal
+unsigned long ind_timeInterval = 0; // Time between last and current detected signals
+
+float ind_freq = 0; // Value to store measurements
+float ind_avg_freq = 0;
+
+int ind_counter = 1;
+
+
+// HUMIDITY DHT11
+#include "DHTStable.h"
+DHTStable DHT;
+#define DHT11_PIN 9
+
+
+// THERMISTORS
+//Librería para logaritmo
+#include <math.h>
+
+// Pines del Arduino
+#define Term_Water A1
+#define Term_Motor A0
+
+float V_ref = 5; //Voltaje del Arduino
+
+//Datos del termistor water
+
+float R_1_W = 5200; //Resistencia fija
+float V_O_W; //Tensión de salida - Medición
+float R_T_W; //Resistencia variable del termistor - Cálculo
+
+int SensorValue_W; //Entrada analógica del Arduino
+
+float T_W; //Temperatura medida - Cálculo
+
+//Parámetros del termistor - Beta model
+float R0_W = 10000; // [Ω]
+float T0_W = 298.15; // [K]
+float B_W = 3435; // [K]
+
+//Datos del termistor motor
+
+float R_1_M = 5088; //Resistencia fija
+float V_O_M; //Tensión de salida - Medición
+float R_T_M; //Resistencia variable del termistor - Cálculo
+
+int SensorValue_M; //Entrada analógica del Arduino
+
+float T_M; //Temperatura medida - Cálculo
+
+//Parámetros del termistor - Beta model
+float R0_M = 1000; // [Ω]
+float T0_M = 298.15; // [K]
+float B_M = 3800; // [K]
+
+
+// ULTRASONICS UB1000
+// General variables
+const int num_med = 1; // average
+const int med_zero = 100; // zero-leveling
+
+// Time variables [ms]
+unsigned long millis_previous = 0;
+unsigned long millis_current;
+const long time_interval = 40; // Around 40
+
+// Define sensor pins
+const int s1 = A3; // Not Bond
+const int s2 = A2; // Bond
+
+// Sensor 1 variables
+float s1_voltage;
+float s1_distance_avg = 0;
+float s1_distance_calibrated = 0;
+float s1_zero_lvl = 0;
+// Sensor 2 variables
+float s2_voltage;
+float s2_distance_avg = 0;
+float s2_distance_calibrated = 0;
+float s2_zero_lvl = 0;
+
+// Manual mapping
+// Not Bond
+const float s1_min_Volt = 176; // [bits]
+const float s1_max_Volt = 887; // [bits]
+const float s1_min_Dist = 89; // [mm]
+const float s1_max_Dist = 983; // [mm]
+// Bond
+const float s2_min_Volt = 176;
+const float s2_max_Volt = 884;
+const float s2_min_Dist = 89;
+const float s2_max_Dist = 990;
+
+// Parámetros de calibración - Revisar Excel
+// Not Bond
+const float s1_slope = 0.998825735;
+const float s1_intercept = 2.344994118;
+// Bond
+const float s2_slope = 1.020507353;
+const float s2_intercept = -2.293051471;
+
+
+// EXTERNAL FUNCTIONS
+// Manual mapping - get decimals - sensor is just which of the 2 sensors we're mapping
+float mapping(float signal, int sensor) {
+  if (sensor == 1) {
+    return (signal - s1_min_Volt) / (s1_max_Volt - s1_min_Volt) * (s1_max_Dist - s1_min_Dist) + s1_min_Dist; // regla de 3
+  }
+  if (sensor == 2) {
+    return (signal - s2_min_Volt) / (s2_max_Volt - s2_min_Volt) * (s2_max_Dist - s2_min_Dist) + s2_min_Dist;
+  }
+  return;
+}
+
+
+// Zero-leveling
+void Zero_Leveling() {
+  for (int i = 0; i < med_zero; i++) {
+    s1_voltage = analogRead(s1);
+    s2_voltage = analogRead(s2);
+    s1_zero_lvl += mapping(s1_voltage, 1);
+    s2_zero_lvl += mapping(s2_voltage, 2);
+    delay(50);
+  }
+  
+  // Zero level setting - Averages
+  s1_zero_lvl /= med_zero;
+  s2_zero_lvl /= med_zero;
+
+  // Calibrate sensors
+  s1_zero_lvl = s1_zero_lvl * s1_slope + s1_intercept;
+  s2_zero_lvl = s2_zero_lvl * s2_slope + s2_intercept;
+
+  Serial.println("Zero levels:");
+  Serial.println(s1_zero_lvl);
+  Serial.println(s2_zero_lvl);
+}
+
+// Inductive
+void Inductive() {
+  // Check the sensor pin
+  if (digitalRead(ind_pin) == HIGH) { // LOW for infrared, HIGH for inductive
+    ind_currentPulseTime = millis(); // Store current time
+
+    // If this is the first pulse detected, initialize the ind_lastPulseTime
+    if (!ind_firstPulseDetected) {
+      ind_firstPulseDetected = true;
+      ind_lastPulseTime = ind_currentPulseTime; // Set the initial pulse time
+      }
+    
+    // Any subsequent pulse
+    else {
+      // Calculate time between pulses
+      ind_timeInterval = ind_currentPulseTime - ind_lastPulseTime;
+      // Update the last pulse time
+      ind_lastPulseTime = ind_currentPulseTime;
+
+      // Calculate frequency and update ind_freq
+      ind_freq += 1000.0 / ind_timeInterval; // Frecuency in Hz
+
+      //Still in-bounds
+      if (ind_counter < ind_measurements) {          
+        ind_counter += 1;
+      }
+      // Out of bounds
+      else {
+        ind_counter = 1; // Reset measurement index
+
+        // Take the average of all measurements
+        ind_avg_freq = ind_freq / ind_measurements;
+        ind_avg_freq = ind_avg_freq * 60; // convert to rpm
+
+        ind_freq = 0; // Reset frequency variable
+      }
+    }
+    delay(200); // Delay to avoid rapid measurements
+  }
+}
+
+// All the measurements
+void All_Measurements() {
+  // ACCELEROMETER
+  mpu.getEvent(&a, &g, &temp);
+
+  //delay(50);
+
+
+  // HUMIDITY
+  // Read data
+  /*
+  int chk = DHT.read11(DHT11_PIN);
+  switch (chk) {
+    case DHTLIB_OK:
+      Serial.print("OK,\t");
+      break;
+    case DHTLIB_ERROR_CHECKSUM:
+      Serial.print("Checksum error,\t");
+      break;
+    case DHTLIB_ERROR_TIMEOUT:
+      Serial.print("Time out error,\t");
+      break;
+    default:
+      Serial.print("Unknown error,\t");
+      break;
+  }
+  */
+  //delay(2000);
+
+  
+  // THERMISTORS
+  //Lectura del Arduino
+  SensorValue_W = analogRead(Term_Water);
+  V_O_W = SensorValue_W * V_ref / 1023;
+
+  SensorValue_M = analogRead(Term_Motor);
+  V_O_M = SensorValue_M * V_ref / 1023; //Mapeo
+
+  //Fórmulas obtenidas
+  R_T_W = R_1_W * V_O_W / (V_ref - V_O_W); //Divisor de tensión
+  T_W = 1/(log(R_T_W/R0_W)/B_W + 1/T0_W) - 273.15; //Ecuación de beta - Conversión a °C
+
+  R_T_M = R_1_M * V_O_M / (V_ref - V_O_M);
+  T_M = 1/(log(R_T_M/R0_M)/B_M + 1/T0_M) - 273.15;
+
+  //delay(2000);
+
+
+  // ULTRASONICS
+  s1_distance_avg = 0;
+  s2_distance_avg = 0;
+
+  s1_voltage = analogRead(s1);
+  s2_voltage = analogRead(s2);
+  s1_distance_avg += mapping(s1_voltage, 1);
+  s2_distance_avg += mapping(s2_voltage, 2);
+  
+  // Get averages
+  s1_distance_avg = s1_distance_avg / num_med;
+  s2_distance_avg = s2_distance_avg / num_med;
+
+  // Calibration
+  s1_distance_calibrated = s1_distance_avg * s1_slope + s1_intercept;
+  s2_distance_calibrated = s2_distance_avg * s2_slope + s2_intercept;
+}
+
+// Print results
+void Print_Results() {
+  // PRINT OUT ALL THE VALUES
+  // First line per sensor is title, delete when actually saving data
+  // Also change all final println to regular print for each sensor (except ultrasonics), all sensors should be within the same line
+
+  // Time
+  Serial.println("Time (s)");
+  Serial.print(float(millis_current) / 1000); // Time in seconds
+  //Serial.print(",");
+  Serial.println("");
+
+  // Accelerometer
+  Serial.println("Acceleration (m^2/s) x, y, z");
+  Serial.print(a.acceleration.x);
+  Serial.print(",");
+  Serial.print(a.acceleration.y);
+  Serial.print(",");
+  Serial.print(a.acceleration.z);
+  //Serial.print(",");
+  Serial.println("");
+  
+  // Inductive
+  Serial.println("Angular Velocity (rpm)");
+  Serial.print(ind_avg_freq);
+  //Serial.print(",");
+  Serial.println("");
+
+  // Humidity
+  Serial.println("Humidity (%), Temperature (°C)");
+  Serial.print(DHT.getHumidity(), 1);
+  Serial.print(",");
+  Serial.print(DHT.getTemperature(), 1);
+  //Serial.print(",");
+  Serial.println("");
+
+  // Thermistors
+  Serial.println("Water_temp, Motor_temp (°C)");
+  Serial.print(T_W);
+  Serial.print(",");
+  Serial.print(T_M);
+  //Serial.print(",");
+  Serial.println("");
+  
+  // Ultrasonics
+  Serial.println("Height (mm) s1, s2");
+  Serial.print(s1_distance_calibrated - s1_zero_lvl);
+  Serial.print(",");
+  Serial.println(s2_distance_calibrated - s2_zero_lvl);
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("There's communication!");
+
+  // ACCELEROMETER
+	// Try to initialize
+	if (!mpu.begin()) {
+		Serial.println("Failed to find MPU6050 chip");
+		//while (1) {
+		//  delay(10);
+		//}
+	}
+	Serial.println("MPU6050 Found!");
+
+	// set accelerometer range to +-8G
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+	// set gyro range to +- 500 deg/s
+	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+	// set filter bandwidth to 21 Hz
+	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+	//delay(100);
+
+
+  // INDUCTIVE
+  pinMode(ind_pin, INPUT_PULLUP); // Sensor pin as input with pull-up resistor
+
+  // HUMIDITY
+  // Nothing to do, yay
+
+
+  // THERMISTORS
+  pinMode(Term_Water, INPUT);
+  pinMode(Term_Motor, INPUT);
+
+
+  // ULTRASONICS
+  pinMode(s1, INPUT);
+  pinMode(s2, INPUT);
+
+
+  // BUTTON
+  pinMode(button_pin, INPUT);
+  pinMode(button_rst, INPUT);
+}
+
+
+
+
+void loop() {
+  // Button state change
+  if (digitalRead(button_pin) == 1) {
+    button_counter += 1;
+    delay(2000); // 2 senconds to change button state
+  }
+
+  // Button Reset
+  if (digitalRead(button_rst) == 1) {
+    button_counter = 0;
+    delay(2000); // 2 seconds to change button state
+  }
+
+
+  // Run the actual codes
+  switch (button_counter) {
+    case 0:
+      Zero_Leveling();
+      break;
+
+    case 1:
+      Inductive();
+      break;
+
+    case 2:
+      // UNIFORM INTERVALS
+      millis_current = millis();
+
+      // Take new measurements only if inside the time interval
+      if (millis_current - millis_previous >= time_interval) {
+        millis_previous = millis_current;
+        All_Measurements();
+        
+        // Print results to csv - With filter for if ultrasonics turn off
+        if (s1_distance_calibrated - s1_zero_lvl < 300 && s2_distance_calibrated - s2_zero_lvl < 300) {
+          Print_Results();
+        }
+      }
+      break;
+
+    default:
+      delay(1000); // Just chill for a bit
+      break;
+  }
+}
+ 
---
+++ Individual Codes/UB1000/Regression/Sine_Regression.py
@@ -0,0 +1,124 @@
+# Sine regression using SciPy - 2 sensors
+import random
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+# Define function to find parameters for
+def function(x, amp, freq, phase, offset):
+    y = amp*np.sin(freq*x + phase) + offset
+    return y
+
+
+# DATA READING AND PREPROCESSING
+# Read the actual data from csv file
+data = pandas.read_csv('KKI_40.csv')
+x_full = np.array(data['Tiempo (s)'].tolist())
+y_full1 = np.array(data['Distancia 1 (mm)'].tolist())
+y_full2 = np.array(data['Distancia 2 (mm)'].tolist())
+
+# Use only some of the data in case of errors
+max_data = 200 # Total data points to use (minus bad_data)
+bad_data = np.where(x_full==5.04)[0][0] # First data points to ignore - Automatic
+
+x_data = x_full[bad_data:max_data-bad_data]
+y_data1 = y_full1[bad_data:max_data-bad_data]
+y_data2 = y_full2[bad_data:max_data-bad_data]
+
+
+
+# REGRESSION
+# Define initial guesses
+# Frequency is the one messing everything up - has to be within 0.0002 of real value
+# Use excel as a lookup table to approximate
+
+amp1 = 25 # [mm]
+freq1 = 6.28 # [rad/s]
+phase1 = 0 # [rad]
+offset1 = 0 # [mm]
+initial1 = (amp1, freq1, phase1, offset1) # For the actual data
+
+amp2 = 25 # [mm]
+freq2 = 6.28 # [rad/s]
+phase2 = 0 # [rad]
+offset2 = 0 # [mm]
+initial2 = (amp2, freq2, phase2, offset2) # For the actual data
+
+
+# Just use a function to get the regression, it's so awesome!
+# popt is what matters, pcov is just about covariance so who cares
+
+popt1, pcov1 = curve_fit(function, x_data, y_data1, initial1)
+popt2, pcov2 = curve_fit(function, x_data, y_data2, initial2)
+
+
+
+# PREDICTION
+# Predict the regressed sine wave
+amp1 = popt1[0]
+freq1 = popt1[1]
+phase1 = popt1[2]
+offset1 = popt1[3]
+
+# Avoid negative amplitudes
+if popt1[0] < 0:
+    amp1 = amp1 * -1
+    phase1 = phase1 + np.pi
+
+# Get phase between 0 and 2pi
+if phase1 > 2*np.pi or phase1 < 0:
+    phase1 = phase1 - phase1//(2*np.pi) * 2*np.pi
+
+y_pred1 = amp1*np.sin(freq1*x_data + phase1) + offset1
+
+amp2 = popt2[0]
+freq2 = popt2[1]
+phase2 = popt2[2]
+offset2 = popt2[3]
+
+if popt2[0] < 0:
+    amp2 = amp2 * -1
+    phase2 = phase2 + np.pi
+
+if phase2 > 2*np.pi or phase2 < 0:
+    phase2 = phase2 - phase2//(2*np.pi) * 2*np.pi
+
+y_pred2 = amp2*np.sin(freq2*x_data + phase2) + offset2
+
+
+# Generate plots
+plt.plot(x_data,y_data1,color='#00FFFF',label='Data')
+plt.plot(x_data,y_pred1,color='#FF007F',label='Regression')
+plt.legend()
+plt.show()
+
+plt.plot(x_data,y_data2,color='#00FFFF',label='Data')
+plt.plot(x_data,y_pred2,color='#FF007F',label='Regression')
+plt.legend()
+plt.show()
+
+
+
+# ACTUAL RESULTS
+# Get averages
+amp = (amp1 + amp2)/2
+freq = (freq1 + freq2) / (4 * np.pi) # in Hz
+
+# Get wavelength
+distance_sensors = 2220 # [mm]
+phase_diff = abs(phase2 - phase1) # [rad]
+
+crests = 2 # Number of crests between sensors
+
+# Correct for multiple wavelengths - Check on the actual wave or guess ranges?
+for i in range(crests - 1):
+    phase_diff += 2*np.pi
+
+wavelength = distance_sensors * 2*np.pi/phase_diff # [mm]
+
+# Display results
+print("Amplitude:", amp, "mm")
+print("Frequency:", freq, "Hz")
+print("Wavelength:", wavelength, "mm")
