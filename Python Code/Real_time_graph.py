fe93e4a65c9aaeef7d215c5a12f7b2132b1d50d0
AndresJimenezPiedra <andres11880jp@estudiantec.cr>
2024-12-04 11:07:28 -0600

    Solve merge conflict

--- Python Code/Real_time_graph.py
+++ Python Code/Real_time_graph.py
@@ -9,26 +9,26 @@ import argparse
 import os
 import statistics
 
+plt.ion()  # turning interactive mode on
+
 port = 'COM9'  # COM9 para Andrés / COM10 para Daniel
 baudrate = 115200
 
 max_measurements = 1000
-graph_maxWaves = 8
+graph_max = 100
 time_csv = [0]
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-Bond_measurements = [0]
+# ax = fig.add_subplot(1, 1, 1)
+Bond_measurements = [550]
 notBond_meausurements = []
-prueba=[]
+fig = plt.plot(time_csv,Bond_measurements)[0]
 
-def animate(a, time_csv, Bond_measurements):
-    Bond_measurements = Bond_measurements[-20:]
-    # notBond_measurments = notBond_measurments[-20:]
+# def animate(a, time_csv, Bond_measurements):
 
-    ax.clear()
-    ax.plot(time_csv, Bond_measurements,'-+')
-    ax.set_xlim(0,np.max(time_csv))
-    ax.set_ylim(0,np.max(Bond_measurements))
+    
+##    Bond_measurements = Bond_measurements[-100:]
+##    time_csv = time_csv[-100:]
+##    # notBond_measurments = notBond_measurments[-20:]
+    
     
 # Wave variables
 max_waves = 400
@@ -44,8 +44,11 @@ except serial.SerialException as e:
     exit()
 
 # Nombre del archivo CSV
-crank_pos = str(input('Crank Position: '))
-motor_freq = input('Motor Frequency (Hz): ')
+##crank_pos = str(input('Crank Position: '))
+##motor_freq = input('Motor Frequency (Hz): ')
+
+crank_pos = 'Z'
+motor_freq = 60
 
 print('\nReady to start measurements!')
 
@@ -57,6 +60,7 @@ csv_filename = csv_path + crank_pos + str(motor_freq) + '.csv'
 # Espera unos segundos para asegurarse de que la conexión esté establecida
 time.sleep(2)
 
+i = 0
 
 # Graph stuff
 update_stat = False # Update graph stats values
@@ -95,8 +99,7 @@ with open(csv_filename, mode='w', newline='') as file:
     # Escribe los encabezados del archivo CSV
     writer.writerow(["Time (s)", "Accel_x (m/s2)", "Accel_y (m/s2)", "Accel_z (m/s2)", "RPM", "Humidity (percentage)", "Amb_Temp (C)", "Water_Temp (C)", "Motor_Temp (C)", "Height 1 (mm)", "Height 2 (mm)"])
 
-    ani = animation.FuncAnimation(fig, animate, fargs=(time_csv, Bond_measurements), interval=40, blit=True, save_count=50, cache_frame_data=False)
-    plt.show()
+   
     try:
         while wave_counter < max_waves:
             if ser.in_waiting > 0:
@@ -108,15 +111,26 @@ with open(csv_filename, mode='w', newline='') as file:
                 if len(data) == 11:
                     # Escribe los datos en el archivo CSV
                     # print(data)
+                    # print("Hello")
                     writer.writerow(data)
-                    print(Bond_measurements)
+                    
 
                     Bond_measurements.append(float(data[10]))
                     time_csv.append(float(data[0]))
                     
                     Bond_measurments = np.asarray(Bond_measurements)
-                    time_csv = np.asarray(time_csv)
+                    # time_csv = np.asarray(time_csv)
                     
+                    # removing the older graph
+                    fig.remove()
+                    
+                    # plotting newer graph
+                    fig = plt.plot(time_csv[-100:],Bond_measurements[-100:],color = 'g')[0]
+                    #
+                    if len(time_csv)>=graph_max:
+                        plt.xlim(time_csv[-graph_max], time_csv[-1])   
+                    # calling pause function for 20 milliseconds
+                    plt.pause(0.02)
 
                     i+=1 
 
@@ -162,6 +176,10 @@ with open(csv_filename, mode='w', newline='') as file:
 
     except KeyboardInterrupt:
         print("Deteniendo la lectura de datos.")
+# ani = animation.FuncAnimation(fig, animate, fargs=(time_csv, Bond_measurements), interval=40, save_count=50, cache_frame_data=False)
+# plt.show()
+# Abre el archivo CSV en modo de escritura
+
 
 
 # Cierra la conexión serie
