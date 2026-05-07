1bb5be728cde1285d1d26de707e26444b842221e
Andrés Jiménez Piedra <andres11880jp@estudiantec.cr>
2026-05-07 10:53:32 -0600

    Uploading Prueba Serial python test 2

--- Python Code/Prueba serial.py
+++ Python Code/Prueba serial.py
@@ -1,23 +1,47 @@
 import serial
 import time
+from pymodbus.client import ModbusSerialClient
+
+client = ModbusSerialClient( 
+    port='COM4',  # Revisar puerto
+    baudrate=2400,
+    parity='N',
+    stopbits=1,
+    bytesize=8,
+    timeout=1
+)
+
+client.connect()
+
+SLAVE = 1
+
+# --- Set frequency (30 Hz) ---
+freq = 1500
+time.sleep(1)
+client.write_register(0x0002, 2000, device_id=SLAVE)
+print('Frequency set')
+
+
 
 ser = serial.Serial('COM3', 115200, timeout=1)
 
-time.sleep(5000)
+time.sleep(5)
 
 data = "1"
 data += "\r\n"
 ser.write(data.encode())
 
 
-time.sleep(5000)
-
 while True:
     try:
         if ser.in_waiting > 0 :
             line = ser.readline().decode('utf-8').strip()
+            if 'Zeros ready' in line:
+                # # --- RUN forward ---
+                client.write_register(0x0001, 1, device_id=SLAVE)
+                print('Start drive')
             print('Line: ', line)
 
     except Exception as e:
         print(e)
-        pass
+        break
